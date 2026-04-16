5951b79f198c9bb963c3622ff5f8b1687af68675
Wojtek <w.20.zmuda@gmail.com>
2026-04-16 12:08:38 +0200

    go

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
+import sys
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 model = "gemini-2.5-flash"
+user_input = sys.argv[1]
+messages=[types.Content(
+    role = "user",
+    parts=[types.Part(text=user_input)],
+)]
+
 
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model=model,
-    contents="Explain how AI works in a few words",
+    contents=messages,
 )
 
 print(response.text)
