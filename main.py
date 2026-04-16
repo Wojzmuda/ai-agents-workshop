66b93d9e5fe97078864badb7554da303dd6cfb8a
wojtek <w.20.zmuda@gmail.com>
2026-04-16 12:13:22 +0200

    s

--- main.py
+++ main.py
@@ -6,18 +6,27 @@ import sys
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 model = "gemini-2.5-flash"
-user_input = sys.argv[1]
-messages=[types.Content(
+
+def main():
+    if len(sys.argv) < 2:
+        print("usage: python3 main.py <prompt>")
+        return
+    user_input = " ".join(sys.argv[1:])
+    messages=[types.Content(
     role = "user",
     parts=[types.Part(text=user_input)],
-)]
+    )]
 
 
-client = genai.Client(api_key=api_key)
+    client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
+    response = client.models.generate_content(
     model=model,
     contents=messages,
-)
+    )
 
-print(response.text)
+    print(response.text)
+    print_token_usage(response)
+
+    if __name__ == "__main__":
+        main()
--- utils.py
+++ utils.py
@@ -4,4 +4,4 @@ def print_token_usage(response: GenerateContentResponse):
     # TODO: Zadanie 2
     if response and response.usage_metadata:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}") # type: ignore
-        print(f"Reponse tokens: {response.usage_metadata.total_token_count}") # type: ignore
+        print(f"Reponse tokens: {response.usage_metadata.candidates_token_count}") # type: ignore
